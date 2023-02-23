ede03efe0c7087c93c5f9cebdefd1fd921d12f83
masac <msktjm98@gmail.com>
2023-02-24 01:38:58 +0900

    material uiの導入まで(12:29)

---
+++ src/components/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from "@mui/material"
+
+const SignIn = () => {
+  const signInWithGoogle = () => {
+    
+  }
+  return (
+    <div>
+      <Button onClick={signInWithGoogle}>グーグルでログインする</Button>
+    </div>
+  )
+}
+
+export default SignIn
